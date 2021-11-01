77b475a307170080524d7ed1a9f295a82f26ce4f
Danila Shkerdin <51801104+danilashkerdin@users.noreply.github.com>
2021-11-01 21:51:50 +0300

    Models changed (name field added)

--- CommentService/CommentServiceApp/models.py
+++ CommentService/CommentServiceApp/models.py
@@ -5,5 +5,4 @@ class Comment(models.Model):
     userID = models.IntegerField()
     name = models.CharField(max_length=50)
     postID = models.IntegerField()
-    name = models.CharField(max_length=50)
     text = models.CharField(max_length=100)
