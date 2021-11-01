de72dd2877ac24f3c94433d671fa2774a4468474
Danila Shkerdin <51801104+danilashkerdin@users.noreply.github.com>
2021-11-01 18:47:15 +0300

    Models changed (name field added)

--- LikeService/LikeServiceApp/models.py
+++ LikeService/LikeServiceApp/models.py
@@ -3,4 +3,5 @@ from django.db import models
 
 class Like(models.Model):
     postID = models.PositiveBigIntegerField()
+    name = models.CharField(max_length=50)
     userID = models.PositiveBigIntegerField()
--- PostService/PostServiceApp/models.py
+++ PostService/PostServiceApp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Post(models.Model):
-    header = models.CharField(max_length=20)
     text = models.CharField(max_length=500)
+    name = models.CharField(max_length=50)
     userID = models.IntegerField()
     timestamp = models.DateTimeField(auto_now=True)
